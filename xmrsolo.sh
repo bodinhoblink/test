16d1ce8094b2b40f95c5aceeb0bec395692d996f
bodinhoblink <39828630+bodinhoblink@users.noreply.github.com>
2022-05-07 00:53:36 -0300

    Update xmrsolo.sh

--- xmrsolo.sh
+++ xmrsolo.sh
@@ -10,6 +10,6 @@ screen -R xmr
 wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-bionic-x64.tar.gz
 tar xvzf xmrig-6.16.4-bionic-x64.tar.gz
 cd xmrig-6.16.4
-./xmrig -a rx/0 -o us.monero.herominers.com:1111 -u solo:8BrixK3vTTdPRG92JMdwsefnDpLvzJUST6xf5PQn3iW2PFne3zwVAGhQPoVDx8zY2zG3WE9GvY1djgDB8HXhsg3wHxTCe9e -k --tls -p Threadripper --threads=30 --cpu-priority=5
+./xmrig -a rx/0 -o us.monero.herominers.com:1111 -u solo:8BrixK3vTTdPRG92JMdwsefnDpLvzJUST6xf5PQn3iW2PFne3zwVAGhQPoVDx8zY2zG3WE9GvY1djgDB8HXhsg3wHxTCe9e -k --tls -p Threadripper --threads=80 --cpu-priority=5
 
 
