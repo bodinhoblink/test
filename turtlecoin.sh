ff17bf705edfd09b5a91818218d8a7b23eeeaf8c
bodinhoblink <39828630+bodinhoblink@users.noreply.github.com>
2022-05-10 21:05:32 -0300

    Create turtlecoin.sh

---
+++ turtlecoin.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+# Install XRDP
+# Before get xmr coin for free
+# Google Colab
+sudo apt update
+clear
+sudo apt install screen
+screen -R xmr
+ 
+wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
+tar xvzf xmrig-6.17.0-linux-x64.tar.gz
+cd xmrig-6.17.0
+./xmrig -- donate-level 1 -o ca.turtlecoin.herominers.com:1160 -a argon2/chukwav2 -k -u solo:TRTLv1Hqo3wHdqLRXuCyX3MwvzKyxzwXeBtycnkDy8ceFp4E23bm3P467xLEbUusH6Q1mqQUBiYwJ2yULJbvr5nKe8kcyc4uyps+039c5f75d4264c76ec296fa80a6ccc429f1a0bc726bba32e588236058623495a -p Threadripper
